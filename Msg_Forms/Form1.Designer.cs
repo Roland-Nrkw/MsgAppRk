d1c910d90c72e240c9bcc7562a5dbdb58de728e3
Roland Kwee <NRKW@eurofins.com>
2020-10-28 17:16:55 +0100

    Add project files.

---
+++ Msg_Forms/Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace Msg_Forms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_ConnectionString = new System.Windows.Forms.TextBox();
+            this.richTextBox_Msgs = new System.Windows.Forms.RichTextBox();
+            this.textBox_Queue = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown_Nr_Msgs = new System.Windows.Forms.NumericUpDown();
+            this.button_Send = new System.Windows.Forms.Button();
+            this.button_Clear = new System.Windows.Forms.Button();
+            this.button_Receive = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_Nr_Msgs)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Connection string:";
+            // 
+            // textBox_ConnectionString
+            // 
+            this.textBox_ConnectionString.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_ConnectionString.Location = new System.Drawing.Point(151, 39);
+            this.textBox_ConnectionString.Name = "textBox_ConnectionString";
+            this.textBox_ConnectionString.Size = new System.Drawing.Size(221, 20);
+            this.textBox_ConnectionString.TabIndex = 1;
+            // 
+            // richTextBox_Msgs
+            // 
+            this.richTextBox_Msgs.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox_Msgs.Location = new System.Drawing.Point(12, 258);
+            this.richTextBox_Msgs.MinimumSize = new System.Drawing.Size(360, 161);
+            this.richTextBox_Msgs.Name = "richTextBox_Msgs";
+            this.richTextBox_Msgs.Size = new System.Drawing.Size(360, 161);
+            this.richTextBox_Msgs.TabIndex = 2;
+            this.richTextBox_Msgs.Text = "";
+            // 
+            // textBox_Queue
+            // 
+            this.textBox_Queue.Location = new System.Drawing.Point(151, 79);
+            this.textBox_Queue.Name = "textBox_Queue";
+            this.textBox_Queue.Size = new System.Drawing.Size(100, 20);
+            this.textBox_Queue.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Queue:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(43, 120);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Nr of Messages:";
+            // 
+            // numericUpDown_Nr_Msgs
+            // 
+            this.numericUpDown_Nr_Msgs.Location = new System.Drawing.Point(151, 118);
+            this.numericUpDown_Nr_Msgs.Name = "numericUpDown_Nr_Msgs";
+            this.numericUpDown_Nr_Msgs.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown_Nr_Msgs.TabIndex = 6;
+            // 
+            // button_Send
+            // 
+            this.button_Send.Location = new System.Drawing.Point(46, 165);
+            this.button_Send.Name = "button_Send";
+            this.button_Send.Size = new System.Drawing.Size(75, 23);
+            this.button_Send.TabIndex = 7;
+            this.button_Send.Text = "Send";
+            this.button_Send.UseVisualStyleBackColor = true;
+            this.button_Send.Click += new System.EventHandler(this.button_Send_Click);
+            // 
+            // button_Clear
+            // 
+            this.button_Clear.Location = new System.Drawing.Point(297, 229);
+            this.button_Clear.Name = "button_Clear";
+            this.button_Clear.Size = new System.Drawing.Size(75, 23);
+            this.button_Clear.TabIndex = 8;
+            this.button_Clear.Text = "clear";
+            this.button_Clear.UseVisualStyleBackColor = true;
+            this.button_Clear.Click += new System.EventHandler(this.button_Clear_Click);
+            // 
+            // button_Receive
+            // 
+            this.button_Receive.Location = new System.Drawing.Point(46, 210);
+            this.button_Receive.Name = "button_Receive";
+            this.button_Receive.Size = new System.Drawing.Size(75, 23);
+            this.button_Receive.TabIndex = 9;
+            this.button_Receive.Text = "Receive";
+            this.button_Receive.UseVisualStyleBackColor = true;
+            this.button_Receive.Click += new System.EventHandler(this.button_Receive_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(24, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(269, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Demo for the Azure Service Bus, course day 3";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(160, 188);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(257, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "buggy, see note in code-behind.Thanks for fixing me!";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 431);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button_Receive);
+            this.Controls.Add(this.button_Clear);
+            this.Controls.Add(this.button_Send);
+            this.Controls.Add(this.numericUpDown_Nr_Msgs);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox_Queue);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.richTextBox_Msgs);
+            this.Controls.Add(this.textBox_ConnectionString);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Test Azure Service Bus Queue";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown_Nr_Msgs)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_ConnectionString;
+        private System.Windows.Forms.RichTextBox richTextBox_Msgs;
+        private System.Windows.Forms.TextBox textBox_Queue;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown_Nr_Msgs;
+        private System.Windows.Forms.Button button_Send;
+        private System.Windows.Forms.Button button_Clear;
+        private System.Windows.Forms.Button button_Receive;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Msg_Forms/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;//Form
+using Microsoft.Azure.ServiceBus;//IQueueClient, Message
+
+/* Edit of MsgApp by Roland Kwee (Eurofins Omegam, Amsterdam, The Netherlands, rolandkwee@eurofins.com) 28 oct 2020:
+ * This is a modification of MsgApp by Maruti Makwana.
+ * I was not happy with the console app.
+ * I believe that console app are something of the past.
+ * A Forms app does not add much lines of code to a console app.
+ * It is only slightly more complex, but offers many advantages, such as a nice GUI.
+ * If you don't like complexity, don't look at Azure, where you have to manage disk, cpu, nic, networking, firewall, monitoring, virtual network, 
+ * queues, service level, plan, zone, region, and a whole lot more even for the smallest cloud app.
+ * 
+ * The only problem is that this forms app does not work.
+ * Probably due to a coding error involving task/async.
+ * Most likely anyone other than the author will spot the error in two seconds.
+ * Thanks for fixing this!
+ * 
+ * 
+ */
+
+namespace Msg_Forms
+{
+    public partial class Form1 : Form
+    {
+        private IQueueClient _queueClient;
+
+        public Form1()
+        {
+            InitializeComponent();
+            textBox_ConnectionString.Text = "Endpoint=sb://[your_connection_string_here];";
+            textBox_Queue.Text = "queue1";
+            numericUpDown_Nr_Msgs.Value = 5;
+        }
+
+        private void button_Clear_Click(object sender, EventArgs e)
+        {
+            richTextBox_Msgs.Text = "";
+        }
+
+        private void button_Send_Click(object sender, EventArgs e)
+        {
+            _queueClient = new QueueClient(textBox_ConnectionString.Text, textBox_Queue.Text);
+            SendAsync().GetAwaiter().GetResult();
+        }
+
+        private void button_Receive_Click(object sender, EventArgs e)
+        {
+            _queueClient = new QueueClient(textBox_ConnectionString.Text, textBox_Queue.Text);
+            ReceiveAsync().GetAwaiter().GetResult();
+        }
+
+        async Task SendAsync()
+        {
+            await SendMessagesAsync((int)numericUpDown_Nr_Msgs.Value);
+            await _queueClient.CloseAsync();
+        }
+
+        async Task ReceiveAsync()
+        {
+            // Register QueueClient's MessageHandler and receive messages in a loop
+            RegisterOnMessageHandlerAndReceiveMessages();
+            await _queueClient.CloseAsync();
+        }
+
+        async Task SendMessagesAsync(int numberOfMessagesToSend)
+        {
+            try
+            {
+                for (int i = 0; i < numberOfMessagesToSend; i++)
+                {
+                    // Create a new message to send to the queue.
+                    string messageBody = $"Message {i}";
+                    Microsoft.Azure.ServiceBus.Message message = new Microsoft.Azure.ServiceBus.Message(Encoding.UTF8.GetBytes(messageBody));
+                    // Write the body of the message to the GUI.
+                    Msg($"Sending message: {messageBody}");
+                    // Send the message to the queue.
+                    await _queueClient.SendAsync(message);
+                }
+                Msg("Sending done.");
+            }
+            catch (Exception exception)
+            {
+                richTextBox_Msgs.Text += $"{DateTime.Now} :: Exception: {exception.Message}\n";
+            }
+        }
+
+        void RegisterOnMessageHandlerAndReceiveMessages()
+        {
+            // Configure the MessageHandler Options in terms of exception handling, number of concurrent messages to deliver etc.
+            MessageHandlerOptions messageHandlerOptions = new MessageHandlerOptions(ExceptionReceivedHandler)
+            {
+                // Maximum number of Concurrent calls to the callback `ProcessMessagesAsync`, set to 1 for simplicity.
+                // Set it according to how many messages the application wants to process in parallel.
+                MaxConcurrentCalls = 1,
+
+                // Indicates whether MessagePump should automatically complete the messages after returning from User Callback.
+                // False below indicates the Complete will be handled by the User Callback as in `ProcessMessagesAsync` below.
+                AutoComplete = false
+            };
+            // Register the function that will process messages
+            _queueClient.RegisterMessageHandler(ProcessMessagesAsync, messageHandlerOptions);
+        }
+
+        async Task ProcessMessagesAsync(Microsoft.Azure.ServiceBus.Message message, CancellationToken token)
+        {
+            // Process the message
+            Msg($"Received message: SequenceNumber:{message.SystemProperties.SequenceNumber} Body:{Encoding.UTF8.GetString(message.Body)}");
+
+            // Complete the message so that it is not received again.
+            // This can be done only if the queueClient is created in ReceiveMode.PeekLock mode (which is default).
+            await _queueClient.CompleteAsync(message.SystemProperties.LockToken);
+
+            // Note: Use the cancellationToken passed as necessary to determine if the queueClient has already been closed.
+            // If queueClient has already been Closed, you may chose to not call CompleteAsync() or AbandonAsync() etc. calls 
+            // to avoid unnecessary exceptions.
+        }
+
+        Task ExceptionReceivedHandler(ExceptionReceivedEventArgs exceptionReceivedEventArgs)
+        {
+            Msg($"Message handler encountered an exception {exceptionReceivedEventArgs.Exception}.");
+            ExceptionReceivedContext context = exceptionReceivedEventArgs.ExceptionReceivedContext;
+            Msg("Exception context for troubleshooting:");
+            Msg($"- Endpoint: {context.Endpoint}");
+            Msg($"- Entity Path: {context.EntityPath}");
+            Msg($"- Executing Action: {context.Action}");
+            return Task.CompletedTask;
+        }
+
+        /// <summary>
+        /// Replacement of Console.WriteLine()
+        /// </summary>
+        /// <param name="S"></param>
+        private void Msg(string S)
+        {
+            richTextBox_Msgs.Text += S + "\n";
+            Update();
+        }
+    }//end class
+}//end ns
